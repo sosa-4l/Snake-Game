df76fc73822889f7f5b106d9d41bc0046391a52a
Belo-Osagie Eseosa <eseosadaboy@gmail.com>
2025-01-13 17:54:06 -0500

    - base functions

--- snake.py
+++ snake.py
@@ -1,1 +1,20 @@
+from tkinter import *
+import _random
 
+class Food():
+    pass
+
+class Snake():
+    pass
+
+def new_turn():
+    pass
+
+def change_dir():
+    pass
+
+def check_collision():
+    pass
+
+def endgame():
+    pass
